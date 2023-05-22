b6d2425f5cae3e8deb93b20be461f0eedf4895bc
AdamHammawa <adamsani2020@gmail.com>
2023-05-22 14:53:57 +0100

    updated testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './testimonials.css'
-import AVT1 from '../../assets/avatar1.jpg'
+import AVT1 from '../../assets/GM.png'
 import AVT2 from '../../assets/avatar2.jpg'
 import AVT3 from '../../assets/avatar3.jpg'
 import AVT4 from'../../assets/avatar4.jpg'
@@ -18,26 +18,30 @@ import 'swiper/css/pagination';
 const data = [
   {
     Avatar: AVT1,
-    name: "Mary John",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, voluptatum!",
+    name: "Mrs. Adunyi Eneojo",
+    post: "GM MetrosourceLTD",
+    review: "Adam is an excellent computer scientist, He was able to give us an appealing website with Great User Experience.",
   },
 
   {
     Avatar: AVT2,
-    name: "Musa Jamilu",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, voluptatum!",
+    name: "Jesse Benedict",
+    post: "CEO Tradapay",
+    review: "Adam is very Professional and Exiciting to work with. He gave me all the options and helped me select the best for my Apps landing Page!",
   },
 
   {
     Avatar: AVT3,
-    name: "Habibu Sani",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, voluptatum!",
+    name: "Hendrixx Lenge",
+    post: "Software Engineer",
+    review: "I worked with the Image compression website Adam Made and I find it straight forward and easy to use. It is an amazing Web App ",
   },
 
   {
     Avatar: AVT4,
-    name: "Maryam Abdullahi",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, voluptatum!",
+    name: "Aliyu Muhammad",
+    post: "Keda IT Officer",
+    review: "It was a Great Pleasure having Adam develop a website for our Organization as it was easy to navigate and appealing to our users",
   },
 
   
@@ -57,13 +61,13 @@ const Testimonials = () => {
       pagination={{ clickable: true }}
       >
         {
-          data.map(({Avatar, name, review}, index) => {
+          data.map(({Avatar, name, post, review}, index) => {
             return (
         <SwiperSlide key={index} className="testimonial">
           <div className="client_image">
             <img src={Avatar} alt="Client Avatar" />
           </div>
-            <h5 className='client_name'> {name} </h5>
+            <h5 className='client_name'> {name} <br /> <small>{post}</small> </h5>
             <small className='client_review'>
               {review}
             </small>
